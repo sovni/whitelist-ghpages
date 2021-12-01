849b1135dcd93025e20fbe019a195c1c11ff42ca
sovni <christophe.burgalat@gmail.com>
2021-12-01 21:59:34 +0100

    Updates

---
+++ precache-manifest.c6d3924f87f9b4d0f9301aaf8f58da2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ff6a4f4b6073a3fce1bb25bc8e31209",
+    "url": "/whitelist-ghpages/index.html"
+  },
+  {
+    "revision": "df8b1d140a561157706d",
+    "url": "/whitelist-ghpages/static/css/2.63f31f8d.chunk.css"
+  },
+  {
+    "revision": "567fccea54fba13fae1e",
+    "url": "/whitelist-ghpages/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "df8b1d140a561157706d",
+    "url": "/whitelist-ghpages/static/js/2.4310dcdf.chunk.js"
+  },
+  {
+    "revision": "567fccea54fba13fae1e",
+    "url": "/whitelist-ghpages/static/js/main.109bb80e.chunk.js"
+  },
+  {
+    "revision": "88108e2026dd1185e75d",
+    "url": "/whitelist-ghpages/static/js/runtime-main.6bad9173.js"
+  }
+]);
